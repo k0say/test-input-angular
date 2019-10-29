a53213f1752cc4e81f4750bdbc7ddce72977b3a9
k0say <kosay@libero.it>
2019-10-29 20:38:50 +0100

    4 files modified

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
   selector: "app-test",
@@ -7,13 +7,17 @@ import { Component, OnInit } from "@angular/core";
 })
 export class TestComponent implements OnInit {
   st: string;
-
+  @Input() childMessage
   constructor() {}
 
   ngOnInit() {}
 
   values = "";
   values2 = "";
+  values3= ""
+  vai() {
+    this.childMessage;
+  }
 
   onKey(value: string) {
     this.values += value + " | ";
@@ -22,5 +26,7 @@ export class TestComponent implements OnInit {
   onClick(value: string)  {
     this.values2 = value.toUpperCase()
   }
+
+
 
 }
